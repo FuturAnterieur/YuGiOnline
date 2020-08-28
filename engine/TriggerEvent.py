c4cd275a7fec383bd917987c0fb072a47d7f3dcd
Futur Anterieur <futuranterieur7@gmail.com>
2020-08-28 10:07:02 -0400

    More file management

---
+++ engine/TriggerEvent.py
@@ -0,0 +1,23 @@
+
+class TriggerEvent:
+    def __init__(self, name, card, effect, triggertype, category, matches):
+        self.card = card
+        self.type = triggertype
+        self.category = category
+        self.name = name
+        self.matches = matches
+        self.funclist = []
+
+    def execute(self, gamestate):
+        for func in self.funclist:
+            func(gamestate)
+
+    def can_be_chained_now(self, gamestate): 
+        if gamestate.is_building_a_chain == False:
+            return False
+        
+        if gamestate.curspellspeed <= 1:
+            return self.effect.spellspeed > gamestate.curspellspeed 
+
+        elif gamestate.curspellspeed > 1:
+            return self.effect.spellspeed >= gamestate.curspellspeed
